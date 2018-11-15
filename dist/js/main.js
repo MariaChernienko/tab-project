7fabdfdde71c7db90576a6fda3adf17a32b0beb8
Maria <murikmarusia@gmail.com>
2018-11-15 16:45:05 +0200

    Added changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,33 +1,26 @@
-const btnHome = document.querySelector('[data-tab = home]');
-const btnProfile = document.querySelector('[data-tab = profile]');
-const btnContact = document.querySelector('[data-tab = contact]');
+// eslint-disable-next-line func-names
+const tab = function () {
+  const buttons = document.querySelectorAll('[data-tab]');
+  const descriptions = document.querySelectorAll('[data-description]');
+  const active = document.querySelectorAll('.active');
 
-const titleHome = document.querySelector('[data-item = home]');
-const titleProfile = document.querySelector('[data-item = profile]');
-const titleContact = document.querySelector('[data-item = contact]');
-
-const items = document.querySelectorAll('.active-title');
-const btn = document.querySelectorAll('[data-tab]');
-
-
-btn.forEach((element) => {
-  element.addEventListener('click', (event) => {
-    items.forEach((element) => {
-      element.classList.remove('active-title');
-      element.classList.add('unactive-title');
+  buttons.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      const curent = element.dataset.tab;
+      tabController(curent);
     });
   });
-});
+  function tabController(curent) {
+    buttons.forEach((element) => {
+      element.classList.remove('active');
+    });
+    document.querySelector(`[data-tab="${curent}"]`).classList.add('active');
+    descriptions.forEach((element) => {
+      element.classList.remove('active');
+    });
+    document.querySelector(`[data-description="${curent}"]`).classList.add('active');
+  }
+};
 
-btnHome.addEventListener('click', (event) => {
-  titleHome.classList.remove('unactive-title');
-  titleHome.classList.add('active-title');
-});
-btnProfile.addEventListener('click', (event) => {
-  titleProfile.classList.remove('unactive-title');
-  titleProfile.classList.add('active-title');
-});
-btnContact.addEventListener('click', (event) => {
-  titleContact.classList.remove('unactive-title');
-  titleContact.classList.add('active-title');
-});
+
+tab();
